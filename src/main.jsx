7052302f7f8e6bd13d05a59386c54df30e5a9714
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-13 16:03:55 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import App from "./App.jsx";
+
+// Bootstrap import
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
