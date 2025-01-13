ae94c156c51b85fd48a5e7dcd940455505cc6daf
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-01-13 16:40:30 +0100

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+
+// pages import
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+
+// layouts import
+import DefaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="about" element={<AboutPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
